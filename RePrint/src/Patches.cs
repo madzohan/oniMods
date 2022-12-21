a11117fdaf29db3a2c3fbcbb3b51610c9cc7af46
Artem Melnyk <madzohan@gmail.com>
2022-12-21 04:24:11 +0200

    feature: Suppress the confirmation popup on "reject all"

--- RePrint/src/Patches.cs
+++ RePrint/src/Patches.cs
@@ -5,8 +5,22 @@ namespace RePrint
     public class Patches
     {
         [HarmonyPatch(typeof(ImmigrantScreen))]
+        [HarmonyPatch("OnSpawn")]
+        public class OnSpawnPatch
+        // Suppress "rejection confirm"
+        {
+            public static void Postfix(ImmigrantScreen __instance)
+            {
+                var actions = Traverse.Create(__instance)
+                    .Field("confirmRejectionBtn").Field("onClick").GetValue();
+                Traverse.Create(__instance).Field("rejectButton").Field("onClick").SetValue(actions);
+            }
+        }
+        
+        [HarmonyPatch(typeof(ImmigrantScreen))]
         [HarmonyPatch("OnRejectionConfirmed")]
-        public class RejectPatch
+        public class OnRejectionConfirmedPatch
+        // Following "rejection confirm" actions with a new ImmigrantScreen generated
         {
             public static void Postfix(ImmigrantScreen __instance)
             {
